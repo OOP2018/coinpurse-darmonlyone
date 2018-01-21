eec262bc8358b344193cc857da8cbe097629f703
hp <manusporn.f@ku.th>
2018-01-21 23:12:24 +0700

    all issue fixed

--- src/coinpurse/MoneyUtil.java
+++ src/coinpurse/MoneyUtil.java
@@ -7,15 +7,18 @@ public class MoneyUtil {
     public static void main(String[] args) {
         List<Coin> coins = new ArrayList<Coin>();
         coins.add( new Coin(20.0, "Dollar"));
-        coins.add( new Coin(20.0, "Dollar"));
+        coins.add( new Coin(20.0, "Bath"));
         coins.add( new Coin(0.5, "Bath"));
         coins.add( new Coin(0.25, "Bath"));
         coins.add( new Coin(1.0, "Bath"));
-        coins.add( new Coin(5.0, "Bath"));
+        coins.add( new Coin(2.0, "Dollar"));
+        coins.add( new Coin(3.0, "Bath"));
+        coins.add( new Coin(4.0, "Bath"));
+        coins.add( new Coin(5.0, "Rubie"));
         printCheckCoin(filterByCurrency(coins,"Bath"));
     }
     public static void printCheckCoin(List<Coin> coins){
-        System.out.println("We have coin");
+        System.out.println("Coin we need");
         for (Coin coin: coins){
             System.out.print(coin + " ");
         }
@@ -23,12 +26,23 @@ public class MoneyUtil {
         sortCoins(coins);
     }
     public static List<Coin> filterByCurrency(List<Coin> coins, String currency){
+        printCoinHave(coins);
+        List<Coin> newCoins = new ArrayList<>();
         for (int i = 0 ; i < coins.size() ; i++){
-            if (!coins.get(i).getCurrency().equals(currency))
-                coins.remove(coins.get(i));
+            if (coins.get(i).getCurrency().equals(currency))
+                newCoins.add(coins.get(i));
         }
-        return coins;
+        return newCoins;
     }
+
+    private static void printCoinHave(List<Coin> coins) {
+        System.out.println("We have coin");
+        for (Coin coin: coins){
+            System.out.print(coin + " ");
+        }
+        System.out.println("");
+    }
+
     public static void sortCoins(List<Coin> coins){
         java.util.Collections.sort(coins);
         System.out.println("After sort ");
