913b006582d1f85e96cf045373ca8736f6ab6de6
hp <manusporn.f@ku.th>
2018-01-30 00:07:59 +0700

    Lab 3 java doc info ValueComparator

--- src/coinpurse/ValueComparator.java
+++ src/coinpurse/ValueComparator.java
@@ -15,16 +15,14 @@ public class ValueComparator implements Comparator<Valuable> {
      * @param a Valuable object to compare with b
      * @param b valuable object
      * @return 1 if have higher value and currency
-     * -1 if have lower value and currency
-     * 0 if both are the same value and currency
+     *          -1 if have lower value and currency
+     *          0 if both are the same value and currency
      */
     public int compare(Valuable a, Valuable b) {
-        final String baht = "Baht";
-        final String dollar = "Dollar";
-
         if (a.getCurrency().equals(b.getCurrency())) {
             return a.getValue() == b.getValue() ? 0 : a.getValue() > b.getValue() ? 1 : -1;
         } else {
+            //Logical; first alphabet A - Z : low - high currency value
             if (a.getCurrency().charAt(0) > b.getCurrency().charAt(0)) {
                 return 1;
             } else {
