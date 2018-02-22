a4c544e99fdbf782b2c01ecafa04d84d524128a7
hp <manusporn.f@ku.th>
2018-02-22 09:40:21 +0700

    Coin improve

--- src/coinpurse/Coin.java
+++ src/coinpurse/Coin.java
@@ -9,7 +9,6 @@ public class Coin extends Money {
     private String valueCurrency;
     /** the value of coin should be*/
     private double beValue;
-    private double coinValue = 1;
     /**
      * Initialize new Coins object
      * @param value amount of the money
@@ -18,24 +17,18 @@ public class Coin extends Money {
     public Coin(double value , String currency){
         super(value,getCountryCurrency()==null ? currency : getCountryCurrency());
         valueCurrency = currency;
-        beValue = value*coinValue;
+        beValue = value;
     }
     /**
-     * Initialize new Coins object
+     * Initialize new Coins object with coinValue
      * @param value amount of the money
      * @param currency brand of value
      */
-    public Coin(double value , String currency,double coinValue){
-        super(value,getCountryCurrency()==null ? currency : getCountryCurrency());
+    public Coin(double value , String currency,double coinValue) {
+        super(value, getCountryCurrency() == null ? currency : getCountryCurrency());
         valueCurrency = currency;
-        this.coinValue = coinValue;
-        beValue = value*coinValue;
+        beValue = value * coinValue;
     }
-    /** set a amount of coinValue*/
-    public void setCoinValue(double coinValue) {
-        this.coinValue = coinValue;
-    }
-
     /**
      * Make a string form an object
      * @return a string representation of the argumen
